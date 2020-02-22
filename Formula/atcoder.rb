ad73cf787bb5cff9a7ad433510d61f88416bac3e
Takumasa Sakao <sakataku7@gmail.com>
2020-02-22 19:38:44 +0900

    Add Formula

---
+++ Formula/atcoder.rb
@@ -0,0 +1,17 @@
+class AtCoder < Formula
+  desc "Unofficial CLI for AtCoder users"
+  homepage "https://github.com/sachaos/atcoder"
+
+  url "https://github.com/sachaos/atcoder/archive/v0.2.2.tar.gz"
+  sha256 "d8d73cf0ab7f067853aa74d5d004f2f278264b981c47eaa6b04653dbba37fc39"
+  head "https://github.com/sachaos/atcoder.git"
+
+  depends_on "go" => :build
+
+  def install
+    (buildpath/"src/github.com/sachaos/atcoder").install buildpath.children
+    cd "src/github.com/sachaos/atcoder" do
+      system "go", "build", "-o", bin/"atcoder"
+    end
+  end
+end
