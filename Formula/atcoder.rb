b40d27c98b6371e950a838312fbb4b9839f51d58
Takumasa Sakao <sakataku7@gmail.com>
2020-02-24 03:26:55 +0900

    Update version to 0.2.5

--- Formula/atcoder.rb
+++ Formula/atcoder.rb
@@ -2,8 +2,8 @@ class Atcoder < Formula
   desc "Unofficial CLI for AtCoder users"
   homepage "https://github.com/sachaos/atcoder"
 
-  url "https://github.com/sachaos/atcoder/archive/v0.2.4.tar.gz"
-  sha256 "0283f9a3a39ba860e7267f8e7217a748d550cbc952c3e686192b3a73439a2c9b"
+  url "https://github.com/sachaos/atcoder/archive/v0.2.5.tar.gz"
+  sha256 "056fb0fc1a3f81eade42941079ce11c212c49e585ccfeaf859a9db62eea6c2ad"
   head "https://github.com/sachaos/atcoder.git"
 
   depends_on "go" => :build
