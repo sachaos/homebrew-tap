22804de5b6620c829edb76447bb6ff46b058c550
Takumasa Sakao <sakataku7@gmail.com>
2020-02-29 22:42:58 +0900

    atcoder 0.2.9-dev.4
    Created by https://github.com/mislav/bump-homebrew-formula-action

--- Formula/atcoder.rb
+++ Formula/atcoder.rb
@@ -2,8 +2,8 @@ class Atcoder < Formula
   desc "Unofficial CLI for AtCoder users"
   homepage "https://github.com/sachaos/atcoder"
 
-  url "https://github.com/sachaos/atcoder/archive/v0.2.7.tar.gz"
-  sha256 "63f2998c341264e3ebcd41c251fe6a885efe5e670686968b0c102c30207ce7ba"
+  url "https://github.com/sachaos/atcoder/archive/v0.2.9-dev.4.tar.gz"
+  sha256 "132fe042c67151d2c5c9a67c16a199370aadc52d767fca0e30fe1c6abe8553f4"
   head "https://github.com/sachaos/atcoder.git"
 
   depends_on "go" => :build
