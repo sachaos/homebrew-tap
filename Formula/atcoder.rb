cb834ac5442a3c4685a06821fd9aa2cdbdaee60a
Takumasa Sakao <sakataku7@gmail.com>
2020-03-01 01:44:26 +0900

    Add build tag

--- Formula/atcoder.rb
+++ Formula/atcoder.rb
@@ -4,6 +4,7 @@ class Atcoder < Formula
 
   url "https://github.com/sachaos/atcoder/archive/v0.2.9.tar.gz"
   sha256 "9577d1722157a75f11dc41e0bfcca317da8360f15b85167e22035492087c802c"
+  version "v0.2.9"
   head "https://github.com/sachaos/atcoder.git"
 
   depends_on "go" => :build
@@ -11,7 +12,7 @@ class Atcoder < Formula
   def install
     (buildpath/"src/github.com/sachaos/atcoder").install buildpath.children
     cd "src/github.com/sachaos/atcoder" do
-      system "go", "build", "-o", bin/"atcoder"
+      system "go", "build", "-o", bin/"atcoder", "-ldflags", "\"-X github.com/sachaos/atcoder/cmd.version=#{version}\""
     end
   end
 end
