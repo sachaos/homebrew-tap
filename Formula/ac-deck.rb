4739d9b0ddb1b6652332582b732919f5be25ac27
goreleaserbot <goreleaser@carlosbecker.com>
2020-06-01 09:25:33 +0900

    Brew formula update for ac-deck version v0.3.3

--- Formula/ac-deck.rb
+++ Formula/ac-deck.rb
@@ -2,16 +2,16 @@
 class AcDeck < Formula
   desc "Unofficial CLI for AtCoder users"
   homepage "https://github.com/sachaos/ac-deck"
-  version "0.3.2"
+  version "0.3.3"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/sachaos/ac-deck/releases/download/v0.3.2/ac-deck_0.3.2_Darwin_x86_64.tar.gz"
-    sha256 "c275498a41b1bd90447cabf55cd26758ffc34af6e211b27dba447b9f4a354fe9"
+    url "https://github.com/sachaos/ac-deck/releases/download/v0.3.3/ac-deck_0.3.3_Darwin_x86_64.tar.gz"
+    sha256 "e1c9489221ecf73228bc5bb8ab52c377ef70ca166f8c846f4588c01c2e8e3103"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/sachaos/ac-deck/releases/download/v0.3.2/ac-deck_0.3.2_Linux_x86_64.tar.gz"
-      sha256 "0cb97d7ee81973ad5796e2cd0c267087ed4526a5b47c8f708aa068d9bf5901a9"
+      url "https://github.com/sachaos/ac-deck/releases/download/v0.3.3/ac-deck_0.3.3_Linux_x86_64.tar.gz"
+      sha256 "1d42c665f4205bc313e8532c8bd1d8b256e885f72cc0e3fdb2ff8cd694f53212"
     end
   end
 
