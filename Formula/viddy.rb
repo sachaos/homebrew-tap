aba45f5e54fd56864adfa6caf0b052f051216fa9
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-17 20:40:45 +0900

    Brew formula update for viddy version v0.1.1

--- Formula/viddy.rb
+++ Formula/viddy.rb
@@ -5,20 +5,20 @@
 class Viddy < Formula
   desc "Modern waach command. Viddy well, gopher. Viddy well."
   homepage "https://github.com/sachaos/viddy"
-  version "0.1.0"
+  version "0.1.1"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/sachaos/viddy/releases/download/v0.1.0/viddy_0.1.0_Darwin_x86_64.tar.gz"
-      sha256 "8181de1242ab4bd7b4d0dd3c60b404e16349657a0b78f28629bb7a4dd6c7a540"
+      url "https://github.com/sachaos/viddy/releases/download/v0.1.1/viddy_0.1.1_Darwin_x86_64.tar.gz"
+      sha256 "4e74e528d4efcbbcdcc562847082a2ae5e0b3055b695bcc49823c2d62f1a188a"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/sachaos/viddy/releases/download/v0.1.0/viddy_0.1.0_Linux_x86_64.tar.gz"
-      sha256 "9ceba27c186205b66f9545292379046df07fa4dfb0c008b8511524d707e9e074"
+      url "https://github.com/sachaos/viddy/releases/download/v0.1.1/viddy_0.1.1_Linux_x86_64.tar.gz"
+      sha256 "f856e520815f39cf126e951ca48843c55f9ff072e2968fdca51146fcc991b3bb"
     end
   end
 
