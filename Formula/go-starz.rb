cd8521cfe47a54ac43f62835970d3ca4fc2370bb
goreleaserbot <goreleaser@carlosbecker.com>
2021-08-25 11:34:56 +0900

    Brew formula update for go-starz version v0.0.5

--- Formula/go-starz.rb
+++ Formula/go-starz.rb
@@ -11,22 +11,26 @@ class GoStarz < Formula
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/sachaos/go-starz/releases/download/v0.0.5/go-starz_0.0.5_darwin_amd64.tar.gz"
-      sha256 "db8a85892b6af5ab0d294c3222c3e07517a2ff4a6404ef3a65f239ba02658c63"
+      sha256 "3b2830c34aa4484c518afbf24bd5025b2cf9e671a1bd21a6aa063ed76a05ba35"
+    end
+    if Hardware::CPU.arm?
+      url "https://github.com/sachaos/go-starz/releases/download/v0.0.5/go-starz_0.0.5_darwin_arm64.tar.gz"
+      sha256 "bbcc75f114638ea3dbf3fc2fd788e9239c75cf5fb326f5cefed373742e537675"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/sachaos/go-starz/releases/download/v0.0.5/go-starz_0.0.5_linux_amd64.tar.gz"
-      sha256 "2f921c3c488d93dd7e99ff871bd6a6256980a8b24f13a0dd77f9d0d7e4283848"
+      sha256 "3f25090620ce3c5bffe4890ad312500b2b9cfe61123a3fc00ec81af3249949ca"
     end
     if Hardware::CPU.arm? && !Hardware::CPU.is_64_bit?
       url "https://github.com/sachaos/go-starz/releases/download/v0.0.5/go-starz_0.0.5_linux_armv6.tar.gz"
-      sha256 "a8d2f65adb9056a8ccf4a6f95a82ff93d2ae5ed037332914f554ee06bf1f6691"
+      sha256 "cefd1ba66fdd001902f1afe242c7bee51c0f5dfde278d1d2fc2516ad3b2b97aa"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/sachaos/go-starz/releases/download/v0.0.5/go-starz_0.0.5_linux_arm64.tar.gz"
-      sha256 "15614cfb2ca022fd17ea97ab46605c9d16589e72c2e1eeaa802ca1c9b47f058c"
+      sha256 "45bac849d71bd5020247c534daec24b4ef11adb700cfbbfac2530e62595b7dfe"
     end
   end
 
